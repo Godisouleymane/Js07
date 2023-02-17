4d3fca93beaea662e91a5e235ea6995bbc1c227f
Godisouleymane <117613536+Godisouleymane@users.noreply.github.com>
2023-02-17 08:36:55 +0100

    Add files via upload
    Projet Js07 piscine codeloccol

---
+++ Js07/squarePerimeter.js
@@ -0,0 +1,15 @@
+module.exports = squarePerimeter
+
+function squarePerimeter(side){
+    const square = {
+        perimeter: side*4,
+        area: side*side,
+    
+        
+}
+
+return square;
+}
+let square = squarePerimeter(4);
+console.log("perimeter :", square.perimeter);
+console.log("area :", square.area);
